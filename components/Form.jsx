dc75ee0b29a21a064e51f5c6e829de46b8fd63fe
Petya Ivanchova <pepivanchova@yahoo.com>
2024-03-28 00:02:45 +0200

    add static form for register and login

---
+++ components/Form.jsx
@@ -0,0 +1,62 @@
+import { EmailOutlined, PasswordOutlined, PersonOutline } from '@mui/icons-material';
+import React from 'react';
+import Link from 'next/link';
+
+const Form = ({type}) => {
+    return (
+        <div className="auth">
+            <div className='content'>
+                <img src='/assets/logo.png' alt="logo" className='logo' />
+
+                <form className='form'>
+                    {type==='register' && (
+                        <div className='input'>
+                            <input
+                                type="text"
+                                placeholder='Username'
+                                className='input-field'
+                            />
+                            <PersonOutline sx={{color: "#737373"}} />
+                        </div>
+                    )}
+
+                    <div className='input'>
+                        <input
+                            type="email"
+                            placeholder='Email'
+                            className='input-field'
+                        />
+                        <EmailOutlined sx={{color: "#737373"}} />
+                        
+                    </div>
+
+                    <div className='input'>
+                        <input
+                            type="password"
+                            placeholder='Password'
+                            className='input-field'
+                        />
+                        <PasswordOutlined sx={{color: "#737373"}} />
+                        
+                    </div>
+
+                    <button className='button' type="submit">
+                        {type === "register" ? "Join Free" : "Let's Chat"}
+                    </button>
+                </form>
+
+                {type === 'register' ? (
+                    <Link href='/' className="link">
+                        <p className='text-center'>Already have an account? Sign In Here</p>
+                    </Link>
+                ) : (
+                    <Link href='/register' className="link">
+                        <p className='text-center'>Don't have an account? Register Here</p>
+                    </Link>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Form;
