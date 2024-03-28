76e2038cfacadf63c7a4adf8c8cc50d909287490
Petya Ivanchova <pepivanchova@yahoo.com>
2024-03-28 14:18:32 +0200

    add sessionProvider, nextauth_secret

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
 import ToasterContext from "@components/ToasterContext";
+import Provider from "@components/Provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-purple-1`}>
+        <Provider>
         <ToasterContext />
-        {children}</body>
+        {children}
+        </Provider>
+        </body>
     </html>
   );
 }
