f2cbaa4c9b23128f02c90d97cacb9e642f02a765
Petya Ivanchova <pepivanchova@yahoo.com>
2024-03-29 14:03:24 +0200

    add edit page with uploading a profileImage

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -13,10 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-blue-2`}>
         <Provider>
           <TopBar />
-          {children}</Provider>
+          {children}
+          </Provider>
         </body>
     </html>
   );
---
+++ app/(root)/profile/page.jsx
@@ -0,0 +1,98 @@
+'use client'
+
+import Loader from "@components/Loader";
+import { PersonOutline } from "@mui/icons-material";
+import { useSession } from "next-auth/react";
+import { CldUploadButton } from "next-cloudinary";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+const Profile = () => {
+    const { data: session, update } = useSession();
+    const user = session?.user;
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (user) {
+            reset({//update your defaultValues && form values
+                username: user?.username,
+                profileImage: user?.profileImage
+            })
+        }
+        setLoading(false);
+    }, [user])
+
+    const {
+        register,
+        watch,
+        setValue,
+        reset,
+        handleSubmit,
+        formState: { error }
+    } = useForm();
+
+    const uploadPhoto = (result) => {
+        setValue("profileImage", result?.info?.secure_url);
+    }
+
+    const updateUser = async (data) => {
+        setLoading(true);
+        try {
+            const res = await fetch(`/api/users/${user._id}/update`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            await update({data});
+
+            setLoading(false);
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    return loading ? <Loader /> : (
+        <div className="profile-page">
+            <h1 className="text-heading3-bold">
+                Edit Your Profile
+            </h1>
+
+            <form className="edit-profile" onSubmit={handleSubmit(updateUser)}>
+                <div className="input">
+                    <input {...register("username", {
+                        required: "Username is required!",
+                        validate: (value) => {
+                            if (value.length < 3) {
+                                return "Username must be at least 3 characters!"
+                            }
+                        }
+                    })}
+                        type="text" placeholder="Username" className="input-field" />
+                    <PersonOutline sx={{ color: "#737373" }} />
+                </div>
+                {error?.username && (
+                    <p className="text-red-500">{error.username.message}</p>
+                )}
+
+                <div className="flex items-center justify-between">
+                    <img src={watch('profileImage') || user?.profileImage || '/assets/person.jpg'}
+                        alt='logo' className="w-40 h-40 rounded-full" />
+                    <CldUploadButton options={{ maxFiles: 1 }} onSuccess={uploadPhoto} uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_PRESET}>
+                        <p className="text-body-bold">Upload new photo</p>
+                    </CldUploadButton>
+                </div>
+
+                <button className="btn" type="submit">
+                    Save Changes
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default Profile;
